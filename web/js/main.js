2548a6e74a865c0844d1321d104d4371f734ae41
Aprendiz <Aprendiz@CALAPRCTIFSP086.FormacionSena.RED>
2021-10-21 17:11:46 -0500

    Generar turnos y excel

--- web/js/main.js
+++ web/js/main.js
@@ -17,26 +17,9 @@ const app = new Vue({
       });
 
       let url = "ajax.php?modulo=Turnos&controlador=Turnos&funcion=crearTurnos&fecha_inicio="+fecha_inicio+"&fecha_fin="+fecha_fin+"&intensidad_horaria="+intensidad_horaria+"&especialidades="+especialidades;
-      //let variable = `&per_id=[${per_id}]`;
-      //url += variable;
+
       console.log(url);
-      //let {data} = await axios.post(url);
-      /*
-      let id = document.getElementById("formulario");
-      const datos = new FormData(id);
-       fetch(url, { method: "POST", body: datos, })
-          .then(function (response) {
-              return response.json();
-          })
-          .then(function (data) {
-              this.datos = data;
-              
-              $("#modal").modal("show");
-          })
-          .catch(function (error) {
-              console.log(error);
-          });
-          console.log(this.datos) */
+
     },
     abrirModal(){
 
