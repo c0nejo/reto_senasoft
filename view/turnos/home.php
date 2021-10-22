5a841520ae108e5781a9a4d8b674950db6d64f3e
JhonatanZ13 <zambranojhonatan20@gmail.com>
2021-10-22 10:17:29 -0500

    :3

--- view/turnos/home.php
+++ view/turnos/home.php
@@ -1,5 +1,5 @@
 <div>
-    <h4 class="display-6">Administrar turnos</h4>
+    <h4 class="display-6">Generar turnos por fechas</h4>
     <hr>
     <div class="row">
         <div class="col-md-8">
@@ -14,9 +14,15 @@
                         <input type="date" name="fecha_fin" class="form-control" id="fecha_fin">
                     </div>
                 </div>
-                <div class="mb-3">
-                    <label class="form-label">Intensidad horaria</label>
-                    <input type="number" class="form-control" name="intensidad_horaria" id="intensidad_horaria">
+                <div class="row">
+                    <div class="col-md-6 mb-3">
+                        <label class="form-label">Intensidad horaria</label>
+                        <input type="number" class="form-control" name="intensidad_horaria" id="intensidad_horaria">
+                    </div>
+                    <div class="col-md-6 mb-3">
+                        <label class="form-label">Personal por turno</label>
+                        <input type="number" class="form-control" name="turno_dia" id="intensidad_horaria">
+                    </div>
                 </div>
                 <div class="mb-3">
                     <label class="form-label">Servicios de salud</label>
@@ -32,36 +38,7 @@
                     </div>
                 </div>
                 <button type="submit" class="btn btn-primary">Generar turnos</button>
-            </form>
-            <!-- <button type="button" class="btn btn-primary" @click="abrirModal()">
-                Launch demo modal
-            </button> -->
-
-            <!-- Modal -->
-            <div class="modal fade" id="modal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div class="modal-dialog modal-xl modal-static">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title" id="exampleModalLabel">Turnos generados</h5>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                        </div>
-                        <div class="modal-body">
-                            <table class="table table-bordered">
-                                <tr>
-                                    <td v-for="(fecha, index) of datos.fechas">{{index}}</td>
-                                </tr>
-                            </table>
-                            <div v-for="(fecha, index) of this.datos.fechas">{{index}}
-
-                            </div>
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                            <button type="button" class="btn btn-primary">Save changes</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            </form> 
             
         </div>
     </div>
